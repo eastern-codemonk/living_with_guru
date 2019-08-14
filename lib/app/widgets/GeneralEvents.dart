8511f5abc14e59c435e2dbd5519d139317dab190
sachin <sachin.kannur@gmail.com>
2019-08-14 18:34:42 +0530

    apk build on travis ci

---
+++ lib/app/widgets/GeneralEvents.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/widgets.dart';
+
+class GeneralEvents extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+  
+}
